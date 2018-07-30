4b34c708d2fac7c4ed1cceb8e86b4f86faf0dfb2
Dico Karssiens <dico.karssiens@gmail.com>
2018-07-31 00:56:59 +0100

    Move to local PC cuz teamviewer connection is sucky rn

---
+++ src/main/kotlin/io/dico/parcels2/GlobalAddedData.kt
@@ -0,0 +1,19 @@
+package io.dico.parcels2
+
+import org.bukkit.OfflinePlayer
+import java.util.*
+
+class GlobalAddedDataManager(val plugin: ParcelsPlugin) {
+
+
+    operator fun get(player: OfflinePlayer): AddedData {
+
+    }
+
+    operator fun get(uuid: UUID): AddedData {
+
+    }
+
+
+}
+
